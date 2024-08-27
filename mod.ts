5a46417b6c21830fb6c7e287af6b3eb30375c93e
Gnlow <gnlow@protonmail.com>
2024-08-27 22:47:33 +0900

    impl arithmetic

---
+++ mod.ts
@@ -0,0 +1,69 @@
+const u = "u"
+const b = "b"
+
+type Tree = (string | Tree)[] 
+
+const operators: string[] = []
+const operands: Tree = []
+
+let state: "u" | "b" = u
+
+const last =
+<A>(as: A[]) =>
+    as[as.length - 1]
+
+const isPrecede =
+(a: string, b: string) =>
+    "+*".indexOf(a) > "+*".indexOf(b)
+
+const input = "+a+b*c;".split("")
+
+input.forEach(char => {
+    console.log("state:", state)
+    console.log(operators)
+    console.log(operands)
+    console.log("input:", char, "\n")
+    if (state == u) {
+        if (char == "+" || char == "*") {
+            operators.push(char)
+        } else if (char == "(") {
+            operators.push(char)
+        } else {
+            operands.push(char)
+            state = b
+        }
+    }
+    if (state == b) {
+        if (char == "+" || char == "*") {
+            const x: Tree = []
+            while (!isPrecede(char, last(operators))) {
+                x.push(operands.pop()!)
+                x.push(operators.pop()!)
+            }
+            if (x.length) operands.push(x.reverse())
+            operators.push(char)
+            state = u
+        } else if (char == ")") {
+            const x: Tree = []
+            while (last(x) != "(") {
+                x.push(operands.pop()!)
+                x.push(operators.pop()!)
+            }
+            operands.push(x)
+        } else if (char == "(") {
+            //func
+            operators.push(char)
+            state = u
+        } else if (char == ";") {
+            while (last(operators)) {
+                const x: Tree = []
+                x.push(operands.pop()!)
+                x.push(operators.pop()!)
+                x.push(operands.pop()!)
+                operands.push(x.reverse())
+            }
+        }
+    }
+})
+
+console.log(operands)
